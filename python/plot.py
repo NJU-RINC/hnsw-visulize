54d2605257ea4b2bbee177d726af57d92af6863e
uestc-lfs <uestc_lfs@outlook.com>
2020-10-15 19:38:28 +0800

    save code

--- python/plot.py
+++ python/plot.py
@@ -1,4 +1,26 @@
 
+from time import sleep
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = [0, 1, 2, 3]
+y = [2, 1, 3, 0]
+z1 = [1, 1, 1, 1]
+z2 = [i + 2 for i in z1]
+
+ax.scatter(x, y, z1)
+plt.show()
+
+sleep(1)
+
+ax.scatter(x, y, z2)
+plt.show()
 
 if __name__ == '_main__':
     pass
